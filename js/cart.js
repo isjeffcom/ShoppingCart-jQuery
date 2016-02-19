5d034e9f24f8745e563febefa228d2fbecd90189
isjeffcom <abbcnnhktv@gmail.com>
2016-02-19 14:12:28 +0000

    Fix

--- js/cart.js
+++ js/cart.js
@@ -15,92 +15,111 @@
     //DOM ready for all
 	$(document).ready(function(){
 	    
-		
-		var proNum = 0;
+		//Statment Ready
 		var items = [];
 		var pprice =[];
-		var cartAll = localStorage.getItem('item');
-        var priceAll = localStorage.getItem('price');
-		
-		//createCart
-		createCart: (function(){
-		
-		//Set LocalStorage Item
-		if(typeof(Storage) !== 'undefined'){
-		        
+		var totalPrice = 0;
+		var cartAll= localStorage.getItem('item');
+        var priceAll = localStorage.getItem('price'); 
 
-			
-		    }else{
-			
-			    alert('Sorry to tell you that your broswer do not support our website.');
-				
-			}
-		}),
+        
+        //check if cartAll == null
+        if(cartAll !== null){
+        	var proNum = JSON.parse(cartAll).length;
+        }else{
+        	proNum = 0;
+        }
+        console.log(proNum);
+		
+
 		
 		
-        //Add function
+        //Add2Cart function
 		
 		$(".add").click(function(){
-		
 		    if(proNum < 10){
 
 			
 				//DOM the name and price from the HTML
 				var mName = $(this).parent().children('.name').html();
 				var mPrice = $(this).parent().children('.price').html();
-				//alert(pprice);
+				var mQty = $(this).parent().children('.qty').val();
+				mQty = parseInt(mQty);
+				console.log("qty: " + mQty);
 				
-				//Product Number Count +1 when click the "add" button
-				proNum++;
-				
+				if(mQty >=0 ){
+					var mPriceCal = mPrice * mQty;
+				}else{
+					alert("Please input a right number");
+					mQty = 1;
+					mPriceCal = mPrice * mQty;
+				}
             	//Add item to HTML localStorage 
-				items.push(mName);
+				items.push(mName,mQty,mPriceCal);
 				localStorage.setItem ('item', JSON.stringify(items));
 					
-				pprice.push(mPrice);
+				pprice.push(mPriceCal);
 				localStorage.setItem ('price', JSON.stringify(pprice));
 
 				//Test alert to check if the function work
-				alert("You Add " + mName + " in cart successful " + " Price: " + mPrice + "  You already choose " +proNum + " products");
+				alert("You Add " + mName + " in cart successful " + " Price: " + mPrice + "  You already have " +proNum + " products");
+				
 		    
 			}else{
 			
-			alert("Sorry, can not add more item");
+				alert("Sorry, can not add more item");
 			
 			}
 		});
 		
+		//Display Cart
 		$(function(){
-            var ppprice = JSON.parse(priceAll);
-			for(var i = 0; i < ppprice.length; i++){
-				
-				var totalPrice = 0;
-				totalPrice += parseFloat(ppprice[i]);
-				alert(totalPrice);
-				
-			}
+			if(priceAll !== null){
+	            var ppprice = JSON.parse(priceAll);
+				for(var i = 0; i < ppprice.length; i++){
+					totalPrice += parseFloat(ppprice[i]);
+					totalPrice = totalPrice.toFixed(2);
+					}
+				console.log(totalPrice);		
+		
+		    }else{
+				ppprice = 0;
+		    }
 			
+		
+
+
+			//Cart Box
+	        
+			if(cartAll != null){
+				$(".cartItem").html("Cart Product: " + cartAll);
+				//$(".cartPrice").html("Price: " + priceAll);
+				$(".totalPrice").html("All Price: " + totalPrice + " $ ");
+
+			}
+		
+		
+		
+		
+		
+			//Clear Cart
+		
+			$(".clearCart").click(function(){
+				
+				alert("clearcart");
+				localStorage.clear();
+				
+			});
+
+		
 		});
-
-
-		//Cart Box
-        
-		if(cartAll != null){
-			$(".cartItem").html(cartAll);
-			$(".cartPrice").html(priceAll);
-			//$(".totalPrice").html(totalPrice);
-		}
-		
-
-		
-		
 		
 		//Check if localStorage oException
 		try {
 		    localStorage.setItem(key, value);
 		}catch(oException){
 			if(oException.name == 'QuotaExceededError'){
-				alert("!!!Exceed Maximum Storage Limit");
+				alert("!!!Exceed Maximum Storage Limit!!!");
 				localStorage.clear();
 			}
 		}
